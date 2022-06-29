4188b552d386f0f5b361083cfac5bb07fde8ddd3
The Secret Door <104723922+The-Secret-Door@users.noreply.github.com>
2022-06-29 10:41:04 +0530

    Create Custom-Template.php

---
+++ Custom-Template.php
@@ -0,0 +1,17 @@
+<div class="row faqpage">	
+	<h3 class="faqh3">
+		FAQ's
+	</h3>
+	     </div>
+          <div class="container">
+    <div class="row">
+		
+<div class="col-lg-6 mb-6">
+
+<?php echo do_shortcode('[sp_easyaccordion id="30346"]'); ?>
+</div>
+<div class="col-lg-6 mb-6">
+<?php echo do_shortcode('[sp_easyaccordion id="30347"]'); ?>
+</div>
+</div>
+</div>
